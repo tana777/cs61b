2065f74008f5e19ea477d8cab3ba5eb461de9971
Tana Gegen <istana.gegen@gmail.com>
2020-06-07 18:04:28 -0700

    add method to LeapYear

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -1,5 +1,5 @@
 /** Class that determines whether or not a year is a leap year.
- *  @author YOUR NAME HERE
+ *  @author Tana Gegen
  */
 public class LeapYear {
 
@@ -12,6 +12,14 @@ public class LeapYear {
         } else {
             System.out.printf("%d is not a leap year.\n", year);
         }
+    }
+
+    public static boolean isLeapYear(int year) {
+
+      if ((year % 400 == 0) || (year % 4 == 0 && year % 100 != 0)) {
+        return true;
+      }
+      return false;
     }
 
     /** Must be provided an integer as a command line argument ARGS. */
@@ -30,4 +38,3 @@ public class LeapYear {
         }
     }
 }
-
