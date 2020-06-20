fb555f5dbb6575d3a9e7f330c359cfe732a7207d
Tana Gegen <istana.gegen@gmail.com>
2020-06-19 18:17:01 -0700

    updates

---
+++ proj1b/Palindrome.java
@@ -0,0 +1,57 @@
+public class Palindrome {
+    public Deque<Character> wordToDeque(String word) {
+        Deque d = new LinkedListDeque();
+        for(int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i);
+            d.addLast(c);
+        }
+        return d;
+    }
+
+
+    public boolean isPalindrome(String word) {
+        if (word == null || word.length() == 0 || word.length() == 1) {
+            return true;
+        }
+        Deque d = wordToDeque(word);
+        int j = 0;
+        if (d.size() % 2 ==0) {
+            j = d.size() / 2;
+        }else {
+            j = (d.size() - 1) / 2;
+        }
+        for (int i = 0; i < j; i++) {
+            if (d.removeLast() == d.removeFirst()) {
+                continue;
+            }else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+    public boolean isPalindrome(String word, CharacterComparator cc) {
+        if (word == null || word.length() == 0 || word.length() == 1) {
+            return true;
+        }
+        Deque d = wordToDeque(word);
+        int j = 0;
+        if (d.size() % 2 ==0) {
+            j = d.size() / 2;
+        }else {
+            j = (d.size() - 1) / 2;
+        }
+        for (int i = 0; i < j; i++) {
+            char x = (char) d.removeFirst();
+            char y = (char) d.removeLast();
+            if (cc.equalChars(x, y)) {
+                continue;
+            }else {
+                return false;
+            }
+        }
+        return true;
+    }
+
+
+}
--- proj1b/TestPalindrome.java
+++ proj1b/TestPalindrome.java
@@ -2,9 +2,11 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 public class TestPalindrome {
-    /*// You must use this palindrome, and not instantiate
+    // You must use this palindrome, and not instantiate
     // new Palindromes, or the autograder might be upset.
     static Palindrome palindrome = new Palindrome();
+
+    static OffByOne offByOne = new OffByOne();
 
     @Test
     public void testWordToDeque() {
@@ -14,5 +16,29 @@ public class TestPalindrome {
             actual += d.removeFirst();
         }
         assertEquals("persiflage", actual);
-    } Uncomment this class once you've created your Palindrome class. */
+    }
+//    Uncomment this class once you've created your Palindrome class.
+
+    @Test
+    public void testisPalindrome() {
+        assertFalse(palindrome.isPalindrome("friday"));
+        assertFalse(palindrome.isPalindrome("BUtTER"));
+        assertTrue(palindrome.isPalindrome("A"));
+        assertTrue(palindrome.isPalindrome(null));
+        assertTrue(palindrome.isPalindrome("kakkak"));
+        assertTrue(palindrome.isPalindrome("reviver"));
+
+    }
+
+    @Test
+    public void testnewisPalindrome() {
+        assertTrue(palindrome.isPalindrome("tops", offByOne));
+        assertTrue(palindrome.isPalindrome("truss", offByOne));
+        assertTrue(palindrome.isPalindrome("", offByOne));
+        assertTrue(palindrome.isPalindrome("a", offByOne));
+        assertFalse(palindrome.isPalindrome("picture", offByOne));
+        assertFalse(palindrome.isPalindrome("Tops", offByOne));
+
+    }
+
 }
