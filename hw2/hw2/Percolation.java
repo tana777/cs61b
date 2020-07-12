56d6dbc1346772c67acfd05b778a7417cc61bb71
Tana Gegen <istana.gegen@gmail.com>
2020-07-11 20:48:15 -0700

    resolve corner case

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -32,7 +32,9 @@ public class Percolation {
         status = new boolean[N * N + 2];
         uf = new WeightedQuickUnionUF(N * N + 2);
         uf2 = new WeightedQuickUnionUF((N * N + 1));
-        connectVirtual();
+        if (len >= 2) {
+            connectVirtual();
+        }
         openSize = 0;
         int num = 0;
         loc = new int[N][N];
@@ -54,9 +56,12 @@ public class Percolation {
             uf.union(virtualtop, i);
             uf2.union(virtualtop, i);
         }
-        for (int i = len * (len - 1); i < len * len; i++) {
-            uf.union(virtualbottom, i);
+        if (len > 2) {
+            for (int i = len * (len - 1); i < len * len; i++) {
+                uf.union(virtualbottom, i);
+            }
         }
+
     }
     /*
         helper method to translate 2D to 1D.
@@ -94,7 +99,8 @@ public class Percolation {
         }
         if (col + 1 < len) {
             int right = xyTo1D(row, col + 1);
-            if (status[right] && (!uf.connected(current, right) || !uf2.connected(current, right))) {
+            if (status[right] && (!uf.connected(current, right)
+                    || !uf2.connected(current, right))) {
                 uf.union(current, right);
                 uf2.union(current, right);
             }
@@ -139,6 +145,9 @@ public class Percolation {
     public boolean isFull(int row, int col) {
         int location = xyTo1D(row, col);
         validate(location);
+        if (len == 1 && status[location]) {
+            return true;
+        }
         return status[location] && uf2.connected(location, virtualtop);
     }
 
@@ -161,6 +170,18 @@ public class Percolation {
 //            }
 //        }
 //        return false;
+        if (len == 1 && openSize == 1) {
+            return true;
+        }
+        else if (len == 1 && openSize == 0) {
+            return false;
+        }
+        else if (len == 2) {
+            for (int i = len; i < len * len; i++) {
+                return uf.connected(virtualtop, i);
+            }
+            return false;
+        }
         return uf.connected(virtualtop, virtualbottom);
     }
 
@@ -168,16 +189,14 @@ public class Percolation {
         use for unit testing (not required)
      */
     public static void main(String[] args) {
-        Percolation rock = new Percolation(3);
+        Percolation rock = new Percolation(2);
         rock.open(0, 0);
         boolean f1 = rock.isFull(0, 0);
-        rock.open(1, 0);
-        boolean f2 = rock.isFull(1, 0);
+        rock.open(1, 1);
+        boolean f2 = rock.isFull(1, 1);
         boolean p1 = rock.percolates();
-        rock.open(2, 0);
-        rock.open(2, 1);
-        boolean f4 = rock.isFull(2, 0);
-        boolean f5 = rock.isFull(2, 1);
+        rock.open(1, 0);
+        boolean f4 = rock.isFull(1, 0);
         boolean p3 = rock.percolates();
     }
 
