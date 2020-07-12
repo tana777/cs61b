1b65de11848e6f5c9671a463d513be215e3a340d
Tana Gegen <istana.gegen@gmail.com>
2020-07-11 18:07:20 -0700

    updates

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -14,18 +14,23 @@ public class Percolation {
         create N-by-N grid, with all sites initially blocked
     */
     private int[][] loc;
-    private int[] status;
+    private boolean[] status;
     private WeightedQuickUnionUF uf;
     private int len;
     private int openSize;
+    private int virtualtop;
+    private int virtualbottom;
 
     public Percolation(int N) {
         if (N <= 0) {
             throw new java.lang.IllegalArgumentException();
         }
+        virtualtop = N * N;
+        virtualbottom = N * N + 1 ;
         len = N;
-        status = new int[N*N];
-        uf = new WeightedQuickUnionUF(N*N);
+        status = new boolean[N*N];
+        uf = new WeightedQuickUnionUF(N*N + 2);
+        connectVirtual();
         openSize = 0;
         int num = 0;
         loc = new int[N][N];
@@ -39,18 +44,23 @@ public class Percolation {
     }
 
     /*
-        helper method to allocate location number for two-dimension array loc.
+        helper method to connect the top row with virtual top
+        and connect the bottom row with virtual bottom.
      */
-    private int[][] assignLoc() {
-        int num = 0;
+    private void connectVirtual() {
         for (int i = 0; i < len; i++) {
-            for (int j = 0; j < len; j++) {
-                loc[i][j] = num;
-                num = num + 1;
-            }
+            uf.union(virtualtop, i);
         }
-        return loc;
+        for (int i = len * (len - 1); i < len * len; i++) {
+            uf.union(virtualbottom, i);
+        }
     }
+
+    /*
+        helper method to solve backwash problem
+     */
+
+
     /*
         helper method to translate 2D to 1D.
      */
@@ -61,18 +71,32 @@ public class Percolation {
     /*
         helper method to check its neighbours' status
      */
-    private void unionNeighbours(int l) {
-        if (l - len >= 0 && status[l - len] == 1 && !uf.connected(l, l - len)) {
-            uf.union(l, l - len);
+    private void unionNeighbours(int row, int col) {
+        int current = xyTo1D(row, col);
+
+        if (row + 1 < len) {
+            int up = xyTo1D(row + 1, col);
+            if (status[up] && !uf.connected(current, up)) {
+                uf.union(current, up);
+            }
         }
-        else if (l + len <= len * len - 1 && status[l + len] == 1 && !uf.connected(l, l + len)) {
-            uf.union(l, l + len);
+        if (row - 1 >= 0) {
+            int down = xyTo1D(row - 1, col);
+            if (status[down] && !uf.connected(current, down)) {
+                uf.union(current, down);
+            }
         }
-        else if (l - 1 >= 0 && status[l - 1] == 1 && !uf.connected(l, l - 1)) {
-            uf.union(l, l - 1);
+        if (col - 1 >= 0) {
+            int left = xyTo1D(row, col - 1);
+            if (status[left] && !uf.connected(current, left)) {
+                uf.union(current, left);
+            }
         }
-        else if (l + 1 <= len * len -1 && status[l + 1] == 1 && !uf.connected(l, l + 1)) {
-            uf.union(l, l + 1);
+        if (col + 1 < len) {
+            int right = xyTo1D(row, col + 1);
+            if (status[right] && !uf.connected(current, right)) {
+                uf.union(current, right);
+            }
         }
     }
 
@@ -84,10 +108,12 @@ public class Percolation {
         if (l < 0 || l > len * len) {
             throw new java.lang.IndexOutOfBoundsException();
         }
-        status[l] = 1;
-        openSize = openSize + 1;
-        unionNeighbours(l);
+        if (!status[l]) {
+            status[l] = true;
+            openSize = openSize + 1;
+            unionNeighbours(row, col);
 
+        }
     }
 
     /*
@@ -98,11 +124,9 @@ public class Percolation {
         if (l < 0 || l > len * len) {
             throw new java.lang.IndexOutOfBoundsException();
         }
-        if(status[l] == 1) {
-            return true;
-        }
-        return false;
+        return status[l];
     }
+
 
     /*
         is the site (row, col) full?
@@ -112,7 +136,16 @@ public class Percolation {
         if (location < 0 || location > len * len) {
             throw new java.lang.IndexOutOfBoundsException();
         }
-        return (location <= len -1) || (uf.find(location) <= len - 1);
+//        if (status[location]) {
+//            for (int l = 0; l < len; l++) {
+//                if (uf.connected(l, location)) {
+//                    return true;
+//                }
+//            }
+//        }
+        return status[location] && uf.connected(location, virtualtop);
+
+
     }
 
     /*
@@ -126,30 +159,31 @@ public class Percolation {
         does the system percolate?
      */
     public boolean percolates(){
-        for (int i = len * (len - 1); i < len * len; i++) {
-            if (uf.find(i) <= len - 1) {
-                return true;
-            }
-        }
-        return false;
+//        for (int up = 0; up < len; up++) {
+//            for (int down = len * (len - 1); down < len * len; down++) {
+//                if (uf.connected(up, down)) {
+//                    return true;
+//                }
+//            }
+//        }
+//        return false;
+        return uf.connected(virtualtop, virtualbottom);
     }
 
     /*
         use for unit testing (not required)
      */
-    public static void main(String[] args) {
-        Percolation rock = new Percolation(3);
-        rock.open(0, 0);
-        boolean f1 = rock.isFull(0, 0);
-        rock.open(0, 1);
-        boolean f2 = rock.isFull(0, 1);
-        boolean p1 = rock.percolates();
-        rock.open(1, 1);
-        boolean p2 = rock.percolates();
-        rock.open(2, 1);
-        boolean p3 = rock.percolates();
-
-
-    }
+//    public static void main(String[] args) {
+//        Percolation rock = new Percolation(3);
+//        rock.open(0, 0);
+//        boolean f1 = rock.isFull(0, 0);
+//        rock.open(0, 1);
+//        boolean f2 = rock.isFull(0, 1);
+//        boolean p1 = rock.percolates();
+//        rock.open(1, 1);
+//        boolean p2 = rock.percolates();
+//        rock.open(2, 1);
+//        boolean p3 = rock.percolates();
+//    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,79 @@
 package hw2;
 
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
+import java.sql.Time;
+
+/** HW 2: Percolation
+ *
+ *  @author tanagegen 07/10/2020
+ *
+ */
+
 public class PercolationStats {
+    /*
+        perform T independent experiments on an N-by-N grid
+     */
+    private int len;
+    private int Times;
+    private Percolation pl;
+    private double[] record;
+
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new java.lang.IllegalArgumentException();
+        }
+        len = N;
+        Times = T;
+        pl = pf.make(len);
+        record = new double[T];
+    }
+
+    /*
+        helper method
+     */
+    public void playGrid() {
+
+        for (int t = 0; t < Times; t++) {
+
+            while (!pl.percolates()) {
+                int row = StdRandom.uniform(0, len);
+                int col = StdRandom.uniform(0, len);
+                pl.open(row, col);
+            }
+            record[t] = pl.numberOfOpenSites() / (len * len);
+        }
+    }
+
+
+
+    /*
+        sample mean of percolation threshold
+     */
+    public double mean() {
+        return StdStats.mean(record);
+    }
+
+    /*
+        sample standard deviation of percolation threshold
+     */
+    public double stddev() {
+        return StdStats.stddev(record);
+
+    }
+
+    /*
+        low endpoint of 95% confidence interval
+     */
+    public double confidenceLow() {
+        return mean() - (1.96 * stddev()) / Math.sqrt(Times);
+    }
+
+    /*
+        high endpoint of 95% confidence interval
+     */
+    public double confidenceHigh() {
+        return mean() + (1.96 * stddev()) / Math.sqrt(Times);
+    }
 
 }
